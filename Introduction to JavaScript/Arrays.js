c99c535e745294ef16bd0a6b63ad44422f060a93
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2022-10-23 12:43:14 -0400

    function practice

--- Introduction to JavaScript/Arrays.js
+++ Introduction to JavaScript/Arrays.js
@@ -1,46 +1,46 @@
 let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 
 // join
-// console.log(fruits.join());
-// console.log(fruits.join('--'));
+console.log(fruits.join());
+console.log(fruits.join('--'));
 
 // push
-// fruits.push('Kiwi');                    // push a string, on to the array
-// console.log(fruits.join());            
-// fruits.push(['Strawberry', 'Blueberry']); // pushed an array on to the array
-// console.log(fruits.join());
-// fruits.push(5);
-// console.log(fruits.join());
+fruits.push('Kiwi');                    // push a string on to the array
+console.log(fruits.join());            
+fruits.push(['Strawberry', 'Blueberry']); // pushed an array on to the array
+console.log(fruits.join());
+fruits.push(5);
+console.log(fruits.join());
 
-//console.log(JSON.stringify(fruits));
+console.log(JSON.stringify(fruits));
 
 // pop
-// console.log(fruits.pop());
-// console.log(fruits.join());
+console.log(fruits.pop());
+console.log(fruits.join());
 
 // shift
-// console.log(fruits.join());
-// console.log(fruits.shift());
-// console.log(fruits.join());
+console.log(fruits.join());
+console.log(fruits.shift());
+console.log(fruits.join());
 
 // unshift
-// console.log(fruits.unshift('Fred'));
-// console.log(fruits.join());
+console.log(fruits.unshift('Fred'));
+console.log(fruits.join());
 
 // concat
-// fruits = fruits.concat(['Strawberry', 'Blueberry']);
-// console.log(JSON.stringify(fruits));
+fruits = fruits.concat('Strawberry', 'Blueberry');
+console.log(JSON.stringify(fruits));
 
 // slice
-// fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-// let citrus = fruits.slice(3);
-// console.log(citrus);
-// console.log(fruits);
+fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let citrus = fruits.slice(3);
+console.log(citrus);
+console.log(fruits);
 
-// fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-// citrus = fruits.slice(1, 3);
-// console.log(citrus);
-// console.log(fruits);
+fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+citrus = fruits.slice(1, 3);
+console.log(citrus);
+console.log(fruits);
 
 
 // splice
