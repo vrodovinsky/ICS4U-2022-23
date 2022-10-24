0105b5d2a61a8ea5fdea16d8915f11e8c85d997f
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2022-10-24 08:35:06 -0400

    coding bat questions

--- Introduction to JavaScript/coding-bat-functional-1.js
+++ Introduction to JavaScript/coding-bat-functional-1.js
@@ -1,7 +1,97 @@
-function aaa(p) {
-    console.log(`Hello, ${p}`);
+function doubling(list) {
+    let result = [];
+    list.forEach(function value(v, i){
+        result[i] = v * 2;
+    });
+    return result;
+}
+let a = [1, 2, 3];
+console.log(doubling(a));
+
+function square(list) {
+    let result = [];
+    list.forEach(function value(v, i){
+        result[i] = v * v ;
+    });
+    return result;
 }
 
-aaa("World");
-aaa(5);
+let a2 = [1, 2, 3];
+console.log(square(a2));
 
+function addStar(list) {
+    let result = [];
+    list.forEach(function value(v, i) {
+        result[i] = v + "*";
+    })
+    return result;
+}
+
+let a3 = ["a", "b", "c"];
+console.log(addStar(a3));
+
+function copies3(list) {
+    let result = [];
+    list.forEach(function value(v, i) {
+        result[i] = v + v + v;
+    })
+    return result;
+}
+
+let a4 = ["aa", "b", "cccc"];
+console.log(copies3(a4));
+
+function moreY(list) {
+    let result = [];
+    list.forEach(function value(v, i) {
+        result[i] = "y" + v + "y";
+    })
+    return result;
+}
+
+let a5 = ["aa", "b", "cccc"];
+console.log(moreY(a5));
+
+function math1(list) {
+    let result = [];
+    list.forEach(function value(v, i) {
+        result[i] = (v + 1) * 10;
+    })
+    return result;
+}
+
+let a6 = [1, 2, 3];
+console.log(math1(a6));
+
+function rightDigit(list) {
+    let result = [];
+    list.forEach(function value(v, i) {
+        result[i] = v%10;
+    })
+    return result;
+}
+
+let a7 = [1, 22, 93];
+console.log(rightDigit(a7));
+
+function lower(list) {
+    let result = [];
+    list.forEach(function value(v, i) {
+        result[i] = v.toLowerCase();
+    })
+    return result;
+}
+
+let a8 = ['HELLO', 'HI'];
+console.log(lower(a8));
+
+function noX(list) {
+    let result = [];
+    list.forEach(function value(v, i) {
+        result[i] = v.replace(/x/g, "");
+    })
+    return result;
+}
+
+let a9 = ["xxax", "xbxbx", "xxcx"]
+console.log(noX(a9));
--- Introduction to JavaScript/coding-bat-map-2.js
+++ Introduction to JavaScript/coding-bat-map-2.js
@@ -10,6 +10,28 @@ let a1 = ["a", "b", "c", "b"]
 
 word0(a1);
 
+function wordCount(x) {
+    let map = {};
+    x.forEach(function mapper(v) {
+         if (map[v]) {
+            map[v] = map[v] + 1;
+        }
+        else {
+            map[v] = 1;
+        }
+    })
+
+    let map2 = {};
+    Object.keys(map).forEach(function mapper2(v) {
+        map2[v] = map[v];
+    });
+     console.log(JSON.stringify(map2));
+}
+
+let a2 = ["a", "b", "c", "b", "c"]
+
+wordCount(a2);
+
 
 function wordLen(x) {
 
@@ -20,9 +42,9 @@ function wordLen(x) {
     console.log(JSON.stringify(map));
 }
 
-let a2 = ["a", "bb", "c", "bbb"]
+let a3 = ["a", "bb", "c", "bbb"]
 
-wordLen(a2);
+wordLen(a3);
 
 
 function wordMultiple(x) {
@@ -45,9 +67,92 @@ function wordMultiple(x) {
      console.log(JSON.stringify(map2));
 }
 
-let a3 = ["a", "b", "a", "c", "c"];
+let a4 = ["a", "b", "a", "c", "c"];
 
-wordMultiple(a3);
+wordMultiple(a4);
 
+function pairs(x) {
+    let map = {};
+    x.forEach(function mapper(v) {
+        map[v.slice(0, 1)] = v.slice(-1);
+    });
+    console.log(JSON.stringify(map));
+}
 
+let a5 = ["man", "moon", "main"];
+
+pairs(a5);
+
+function firstChar(x) {
+    let map = {};
+    x.forEach(function mapper(v) {
+        if (map[v.charAt(0)])
+            map[v.charAt(0)] = map[v.charAt(0)] + v;
+        else {
+            map[v.charAt(0)] = v;
+        }
+    });
+    console.log(JSON.stringify(map));
+}
+
+let a6 = ["salt", "tea", "soda", "toast"];
+firstChar(a6);
+
+function wordAppend(x) {
+    let map = {};
+    let result = "";
+    x.forEach(function mapper(v) {
+        if (map[v]) {
+            map[v] = map[v] + 1;
+        }
+        else {
+            map[v] = 1;
+        }
+
+        if (map[v] % 2 == 0)
+        result += v;
+    });
+    console.log(result);
+}
+
+let a7 = ["a", "b", "a", "c", "a", "d", "a"];
+wordAppend(a7);
+
+function allSwap(x) {
+    let map = new Map();
+    x.forEach(function mapper(v, i, a) {
+        if ( map.has(v.charAt(0)) ) {
+            let temp = a[map.get(v.charAt(0))];
+            a[map.get(v.charAt(0))] = v;
+            a[i] = temp;
+            map.delete(v.charAt(0));
+        } else {
+            map.set(v.charAt(0), i);
+        }
+    });
+    console.log(x);
+}
+
+let a8 = ["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"];
+allSwap(a8);
+
+function firstSwap(x) {
+    let map = new Map();
+    x.forEach(function mapper(v, i, a) {
+        if ( map.has(v.charAt(0)) && map.get(v.charAt(0)) != -1 ) {
+            let temp = a[map.get(v.charAt(0))];
+            a[map.get(v.charAt(0))] = v;
+            a[i] = temp;
+            map.set(v.charAt(0), -1);
+        } else {
+            if (!map.has(v.charAt(0))) {
+                map.set(v.charAt(0), i);
+            }
+        }
+    });
+    console.log(x);
+}
+
+let a9 = ["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"];
+firstSwap(a9);
 
