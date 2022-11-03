2618abd55f50764af159754433dc07e9f147adfc
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2022-11-02 21:31:00 -0400

    teams

---
+++ Sports Statistics Application/script.js
@@ -0,0 +1,146 @@
+let teams = []
+
+let team = {};
+    team['name'] = 'Atlanta Hawks';
+    team['id'] = 1;
+    team['W'] = 4;
+    team['L'] = 3;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+    
+
+let team = {};
+    team['name'] = 'Boston Celtics';
+    team['id'] = 2;
+    team['w'] = 4;
+    team['L'] = 2;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'Chicago Bulls';
+    team['id'] = 3;
+    team['w'] = 4;
+    team['L'] = 4;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'Golden State Warriors';
+    team['id'] = 4;
+    team['w'] = 3;
+    team['L'] = 5;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'LA Clippers';
+    team['id'] = 5;
+    team['w'] = 3;
+    team['L'] = 4;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'Maimi Heat';
+    team['id'] = 6;
+    team['w'] = 3;
+    team['L'] = 5;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'Brooklyn Nets';
+    team['id'] = 7;
+    team['w'] = 2;
+    team['L'] = 6;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'New York Knicks';
+    team['id'] = 8;
+    team['w'] = 3;
+    team['L'] = 3;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'Toronto Raptors';
+    team['id'] = 9;
+    team['w'] = 4;
+    team['L'] = 3;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+let team = {};
+    team['name'] = 'Los Angelos Lakers';
+    team['id'] = 10;
+    team['w'] = 1;
+    team['L'] = 5;
+    team['T'] = 0;
+    team['games'] = [{
+        'home': true,
+        'date': '',
+        'opp': 0,
+        'score': [0, 0]
+    }]
+teams.push(team);
+
+
+    
+
