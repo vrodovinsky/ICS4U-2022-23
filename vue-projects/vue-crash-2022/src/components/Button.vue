4715282669ccdd760934bc18cda784d34be6de11
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2023-01-27 08:33:35 -0500

    view.js video

---
+++ vue-projects/vue-crash-2022/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="onClick()" :style="{background: colour}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default ({
+    name: 'Button',
+    props: {
+        text: String,
+        colour: String
+    },
+    methods: {
+        onClick() {
+            this.$emit('btn-click')
+        }
+    }
+})
+</script>
---
+++ vue-projects/vue-crash-2022/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @btn-click="$emit('tpggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task' " :colour="showAddTask ? 'red' : 'green' "/>
+    
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default({
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        Button
+    }
+})
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
