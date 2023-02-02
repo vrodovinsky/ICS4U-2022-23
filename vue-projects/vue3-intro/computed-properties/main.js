cb71824891a6a1991a34c627ef1d01285daa5db6
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2023-02-02 11:14:45 -0500

    intro to view.js

--- vue-projects/vue3-intro/computed-properties/main.js
+++ vue-projects/vue3-intro/computed-properties/main.js
@@ -1,15 +1,14 @@
 const app = Vue.createApp({
    data() {
       return {
+         selectedVariant: 0,
          cart: 0,
          product: 'Socks',
          brand: 'Vue Mastery',
-         image: './assets/images/socks_blue.jpg',
-         inStock: false,
          details: ['50% cotton', '30% wool', '20% polyester'],
          variants: [
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
          ]
       }
    },
@@ -17,8 +16,22 @@ const app = Vue.createApp({
       addToCart() {
          this.cart += 1
       },
-      updateImage(variantImage) {
-         this.image = variantImage
+      updateVariant(index) {
+         this.selectedVariant = index
+      }
+   },
+   computed: {
+      title() {
+         return this.brand + " " + this.product
+      },
+      image() {
+         return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+         return this.variants[this.selectedVariant].quantity
+      },
+      onSale() {
+         return this.variants[this.selectedVariant].onSale ? "On sale" : ""
       }
    }
 })
