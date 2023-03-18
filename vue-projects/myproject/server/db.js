6d68b5b02568ef15a28de32da83073b881c87730
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2023-03-18 09:53:59 -0400

    mongoDB

---
+++ vue-projects/myproject/server/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('Database connected')).catch((err) => console.log(err))
